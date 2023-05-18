d4309f99599abfe6569cb354023d475628874923
anurag000001 <132413677+anurag000001@users.noreply.github.com>
2023-05-19 04:18:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6089756661:AAGZLVjlY1UsKVzfgz9uSJll4w58McbFP6A")
+    API_ID = int(os.environ.get("API_ID", "27881923"))
+    API_HASH = os.environ.get("API_HASH", "79abda5e46a51fc0dce1313f2548ce19")
+    AUTH_USERS = ""eca3a991085702")
+    AUTH_USERS = ""
